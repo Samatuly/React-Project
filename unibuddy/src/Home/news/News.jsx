1471f4b3f25c5b47d8cce5b0992e860d41aef6a5
Walparis <walparis1@mail.ru>
2023-10-21 01:26:11 +0600

    changed the look of top-share part

--- unibuddy/src/Home/news/News.jsx
+++ unibuddy/src/Home/news/News.jsx
@@ -1,40 +1,62 @@
 import React from "react";
 import "./news.css";
-import {EmojiEmotions, Label, PermMedia, Person, Room, Share, Telegram} from '@mui/icons-material';
+import {
+  EmojiEmotions,
+  Label,
+  PermMedia,
+  Person,
+  Room,
+  Share,
+  Telegram,
+} from "@mui/icons-material";
 
-function News(){
-    return(
-        <>
-            <div className="news-container">
-                <div className="news-wrapper">
-                    <div className="news-top">
-                        <h1>HomePage</h1>
-                    </div>
-                    <div className="news-bottom">
-                        <div className="news-options">
-                            <button className="news-button"><PermMedia className="news-icon"/></button>
-                                <button className="news-button"><Label className="news-icon"/></button>
-                                    <button className="news-button"><Room className="news-icon"/></button>
-                                        <button className="news-button"><EmojiEmotions className="news-icon"/></button>
-                        </div>
-                        <button className="news-button"><Telegram/></button>
-      
-//                         <Person/>
-//                         <input placeholder="What's in your mind? " className="news-input"/>
-//                     </div>
-//                     <hr className="news-hr"/>
-//                     <div className="news-bottom">
-//                         <div className="news-options">
-//                             <PermMedia className="news-icon"/>
-//                             <Label className="news-icon"/>
-//                             <Room className="news-icon"/>
-//                             <EmojiEmotions className="news-icon"/>
-//                         </div>
-//                         <button className="news-button"><Telegram/>Share</button>
-                    </div>
-                </div>
+function News() {
+  return (
+    <>
+      <div className="news-container">
+        <div className="news-wrapper">
+          <div className="news-top">
+            <h1>HomePage</h1>
+          </div>
+          <div className="news-new-post">
+            <Person />
+            <input placeholder="What's in your mind? " className="news-input" />
+          </div>
+          <div className="news-bottom">
+            <div className="news-options">
+              <button className="news-button">
+                <PermMedia className="news-icon" />
+              </button>
+              <button className="news-button">
+                <Label className="news-icon" />
+              </button>
+              <button className="news-button">
+                <Room className="news-icon" />
+              </button>
+              <button className="news-button">
+                <EmojiEmotions className="news-icon" />
+              </button>
+              <button className="news-button">
+                <Telegram className="news-icon" />
+              </button>
             </div>
-        </>
-    );
+          </div>
+          {/* <hr className="news-hr" />
+          <div className="news-bottom">
+            <div className="news-options">
+              <PermMedia className="news-icon" />
+              <Label className="news-icon" />
+              <Room className="news-icon" />
+              <EmojiEmotions className="news-icon" />
+            </div>
+            <button className="news-button">
+              <Telegram />
+              Share
+            </button>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
 }
 export default News;
