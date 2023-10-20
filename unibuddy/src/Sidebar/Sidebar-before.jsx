2c12a0f3e14731081f6569d5fe92e63877c0a799
Walparis <walparis1@mail.ru>
2023-10-21 01:28:20 +0600

    refactor

--- unibuddy/src/Sidebar/Sidebar-before.jsx
+++ unibuddy/src/Sidebar/Sidebar-before.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import './sidebar.css';
-import {SidebarData_before} from "./SidebarData_before"
+import "./sidebar.css";
+import { SidebarData_before } from "./SidebarData_before";
 
-function Sidebar(){
-    return(
-        <div className="sidebar">
-            <ul className="sidebarList">
-                {SidebarData_before.map((val, key) => {
-                    return <li className="row"
-                               id={window.location.pathname == val.link ? "active" : ""}
-                               key = {key} onClick={() => {window.location.pathname = val.link}}>
-                        <div className="sidebar-icon">{val.icon}</div>
-                        {" "}
-                        <div>{val.title}</div>
-                    </li>;
-                })}
-            </ul>
-        </div>
-    )
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul className="sidebarList">
+        {SidebarData_before.map((val, key) => {
+          return (
+            <li
+              className="row"
+              id={window.location.pathname == val.link ? "active" : ""}
+              key={key}
+              onClick={() => {
+                window.location.pathname = val.link;
+              }}
+            >
+              <div className="sidebar-icon">{val.icon}</div>{" "}
+              <div>{val.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
