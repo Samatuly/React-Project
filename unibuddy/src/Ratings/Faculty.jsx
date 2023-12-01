e24d557c1089772dfce4bdf8425d37cb4cfe1c26
Madi <walparis1@mail.com>
2023-12-01 14:26:06 +0600

    made image clickable on faculty page

--- unibuddy/src/Ratings/Faculty.jsx
+++ unibuddy/src/Ratings/Faculty.jsx
@@ -1,43 +1,43 @@
 import React from "react";
-import './ratings.css'
+import "./ratings.css";
 import { Link } from "react-router-dom";
 
-function Faculty(){
-    return(
-        <div className='faculty'>
-            <div className="faculty-grid">
-                <div className="faculty-item">
-                    <Link className="link" to = "/professors">
-                        <h4>School of Mathematics and Cybernetics</h4>
-                    </Link>
-                </div>
-                <div className="faculty-item">
-                    <Link className="link" to = "/professors">
-                        <h4>School of Information Technology and Engineering</h4>
-                    </Link>
-                </div>
-                <div className="faculty-item">
-                    <Link className="link" to = "/professors">
-                        <h4>Business School</h4>
-                    </Link>
-                </div>
-                <div className="faculty-item">
-                    <Link className="link" to = "/professors">
-                        <h4>Kazakhstan Maritime Academy</h4>
-                    </Link>
-                </div>
-                <div className="faculty-item">
-                    <Link className="link" to = "/professors">
-                        <h4>International School of Economics</h4>
-                    </Link>
-                </div>
-                <div className="faculty-item">
-                    <Link className="link" to = "/professors">
-                        <h4>School of Chemical Engineering</h4>
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )
+function Faculty() {
+  return (
+    <div className="faculty">
+      <div className="faculty-grid">
+        <Link className="link" to="/professors">
+          <div className="faculty-item">
+            <h4>School of Mathematics and Cybernetics</h4>
+          </div>
+        </Link>
+        <Link className="link" to="/professors">
+          <div className="faculty-item">
+            <h4>School of Information Technology and Engineering</h4>
+          </div>
+        </Link>
+        <Link className="link" to="/professors">
+          <div className="faculty-item">
+            <h4>Business School</h4>
+          </div>
+        </Link>
+        <Link className="link" to="/professors">
+          <div className="faculty-item">
+            <h4>Kazakhstan Maritime Academy</h4>
+          </div>
+        </Link>
+        <Link className="link" to="/professors">
+          <div className="faculty-item">
+            <h4>International School of Economics</h4>
+          </div>
+        </Link>
+        <Link className="link" to="/professors">
+          <div className="faculty-item">
+            <h4>School of Chemical Engineering</h4>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 export default Faculty;
