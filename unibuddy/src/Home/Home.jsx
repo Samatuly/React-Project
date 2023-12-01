1087c3239f33b319b3a8857ac1100cbbb261430f
Madi <walparis1@mail.com>
2023-12-01 21:18:36 +0600

    1

--- unibuddy/src/Home/Home.jsx
+++ unibuddy/src/Home/Home.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import './home.css'
+import "./home.css";
 import Share from "./Share/Share";
 import RightBar from "./RightBar/RightBar";
 import Sidebar from "../Sidebar/Sidebar";
-
-function Home(){
-    return(
-        <>
-            <div className="home-container">
-                <Share/>
-                <RightBar/>
-            </div>
-        </>
-    );
+function Home() {
+  return (
+    <>
+      <div className="home-container">
+        <Share />
+        <RightBar />
+      </div>
+    </>
+  );
 }
 export default Home;
