f17b21271f88b6c6c323701a5b4a0a5057325f0b
Madi <walparis1@mail.com>
2023-12-01 21:19:00 +0600

    formatting

--- unibuddy/src/Login/Login.jsx
+++ unibuddy/src/Login/Login.jsx
@@ -1,58 +1,62 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import './Login.css'
-import { auth } from '../Firebase/Firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import loginPhoto from '../Login/Login_page_photo.jpg';
+import "./Login.css";
+import { auth } from "../Firebase/Firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import loginPhoto from "../Login/Login_page_photo.jpg";
 
 const SignIn = (props) => {
-    const history = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+  const history = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const login = (e) => {
-        e.preventDefault();
-        if (!email || !password) {
-            setError("Please fill in both email and password.");
-            return;
-        }
+  const login = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Please fill in both email and password.");
+      return;
+    }
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                props.onSignIn();
-                history('/home');
-            })
-            .catch((error) => {
-              setError("Invalid email or password. Please try again.");
-            });
-    };
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        props.onSignIn();
+        history("/home");
+      })
+      .catch((error) => {
+        setError("Invalid email or password. Please try again.");
+      });
+  };
 
-    return (
-        <div className="auth-container">
-            <img src={loginPhoto} alt="Login" width='60%'/>
-            <h2>Login</h2>
-            <form onSubmit={login} className="login_form">
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+  return (
+    <div className="auth-container">
+      <img src={loginPhoto} alt="Login" width="60%" />
+      <h2>Login</h2>
+      <form onSubmit={login} className="login_form">
+        <input
+          type="text"
+          placeholder="Username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-                <button className="login-button" type="submit">Login</button>
-                {error && <div className="error">{error}</div>}
-                <Link to="/register" className="link-login">Don't have an account? Register here.</Link>
-            </form>
-        </div>
-    );
-}
+        <button className="login-button" type="submit">
+          Login
+        </button>
+        {error && <div className="error">{error}</div>}
+        <Link to="/register" className="link-login">
+          Don't have an account? Register here.
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default SignIn;
