b669e97f3ea1a167bbba913a43d9b5056a8cb75a
Madi <walparis1@mail.com>
2023-12-03 17:23:25 +0600

    ability to favorite books and filter with buttons

--- unibuddy/src/E-Library/BookDetail.jsx
+++ unibuddy/src/E-Library/BookDetail.jsx
@@ -1,17 +1,28 @@
 // BookDetail.js
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { collection, doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  query,
+  where,
+} from "firebase/firestore";
 import { firestore } from "../Firebase/Firebase";
-import './BookDetail.css';
+import "./BookDetail.css";
+import { useAuthDetails } from "../Login/useAuthDetailsHook";
+import { update } from "firebase/database";
 
 const BookDetail = () => {
   const { bookId } = useParams();
   const [book, setBook] = useState(null);
+  const { authUser, userSignOut } = useAuthDetails();
 
   useEffect(() => {
     const fetchBookDetails = async () => {
-      const bookDocRef = doc(firestore, 'books', bookId);
+      const bookDocRef = doc(firestore, "books", bookId);
       const bookDoc = await getDoc(bookDocRef);
       if (bookDoc.exists()) {
         setBook({
@@ -28,14 +39,55 @@ const BookDetail = () => {
 
   if (!book) {
     // Book deatil not fetched case
-    return <div className='Loading'>Loading...</div>;
+    return <div className="Loading">Loading...</div>;
   }
+  const addToFavorites = async (bookId, bookName) => {
+    try {
+      const userCollection = collection(firestore, "users");
+      const userQuery = query(
+        userCollection,
+        where("userId", "==", authUser.uid)
+      );
+      const userQuerySnapshot = await getDocs(userQuery);
+      if (!userQuerySnapshot.empty) {
+        const userDoc = userQuerySnapshot.docs[0];
+        const userFavorites = userDoc.data().favoriteBooks || [];
+        if (userFavorites.includes(bookName)) {
+          console.log("User already favorites this book");
+        } else {
+          const updatedFavorites = [...userFavorites, bookName];
+          const updDocRef = doc(userCollection, userDoc.id);
+          await updateDoc(updDocRef, { favoriteBooks: updatedFavorites });
 
+          // add userId to book db also
+          const bookRef = doc(firestore, "books", bookId);
+          const bookSnap = await getDoc(bookRef);
+          if (!bookSnap.empty) {
+            await updateDoc(bookRef, {
+              whoTook: [...bookSnap.data().whoTook, authUser.uid],
+            });
+          }
+          console.log("Book successfully added to favorites");
+        }
+      } else {
+        console.log("User not found with the specified UID.");
+      }
+    } catch (err) {
+      console.error("Error updating favorites: ", err.message);
+    }
+  };
   return (
-    <div className='bookDetail'>
+    <div className="bookDetail">
+      <img src={book.image}></img>
       <h1>{book.title}</h1>
       <p>{book.author}</p>
       <p>{book.year}</p>
+      <button
+        className="book-detail-favorite"
+        onClick={() => addToFavorites(book.id, book.title)}
+      >
+        Add to favorites
+      </button>
       {/* Add more details as needed */}
     </div>
   );
--- unibuddy/src/E-Library/E-Library.jsx
+++ unibuddy/src/E-Library/E-Library.jsx
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './E-Library.css';
-import { collection, getDocs } from 'firebase/firestore';
+import "./E-Library.css";
+import { collection, getDocs } from "firebase/firestore";
 import { db, firestore } from "../Firebase/Firebase";
 import BookDetail from "./BookDetail";
 import { books } from "./libraryData";
+import { useAuthDetails } from "../Login/useAuthDetailsHook";
 
 const E_Library = (props) => {
   const [booksData, setBooksData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-
+  const [searchQuery, setSearchQuery] = useState("");
+  const [activeFilterButton, setActiveFilterButton] = useState("all");
+  const { authUser, userSignOut } = useAuthDetails();
   useEffect(() => {
     const fetchBooks = async () => {
-      const bookCollection = collection(firestore, 'books');
+      const bookCollection = collection(firestore, "books");
       const booksSnapshot = await getDocs(bookCollection);
-      const bookData = booksSnapshot.docs.map(doc => ({
+      const bookData = booksSnapshot.docs.map((doc) => ({
         id: doc.id,
-        ...doc.data()
+        ...doc.data(),
       }));
       setBooksData(bookData);
     };
 
     fetchBooks();
   }, []);
-
+  const filterBooks = () => {
+    if (activeFilterButton === "all") {
+      return booksData;
+    } else if (activeFilterButton === "favorite") {
+      // Assuming you have a user ID, and organizations have a "members" field
+      return booksData.filter((book) => book.whoTook.includes(authUser.uid));
+    }
+  };
+  const handleButtonClick = (buttonType) => {
+    setActiveFilterButton(buttonType);
+  };
   return (
     <div className="library">
       <input
@@ -34,16 +46,27 @@ const E_Library = (props) => {
         onChange={(e) => setSearchQuery(e.target.value)}
       />
       <div>
-        <button className="library-button filter">All</button>
-        <button className="library-button filter">Favourites</button>
+        <button
+          className="library-button filter"
+          onClick={() => handleButtonClick("all")}
+        >
+          All
+        </button>
+        <button
+          className="library-button filter"
+          onClick={() => handleButtonClick("favorite")}
+        >
+          Favourites
+        </button>
       </div>
       <div className="library-list">
-        {booksData
+        {filterBooks()
           .filter((book) => {
-            const searchTerms = searchQuery.toLowerCase().split(' ');
-            return searchTerms.every((term) =>
-              book.title.toLowerCase().includes(term) ||
-              book.author.toLowerCase().includes(term)
+            const searchTerms = searchQuery.toLowerCase().split(" ");
+            return searchTerms.every(
+              (term) =>
+                book.title.toLowerCase().includes(term) ||
+                book.author.toLowerCase().includes(term)
             );
           })
           .map((book) => (
