987467fd05c760263ab5e4e7e7fa4f78cee0fde7
Madi <walparis1@mail.com>
2023-12-18 20:53:31 +0600

    1

--- unibuddy/src/Home/Share/Share.jsx
+++ unibuddy/src/Home/Share/Share.jsx
@@ -11,7 +11,7 @@ import {
 } from "@mui/icons-material";
 function Share({ searchTerm }) {
   const [isFormOpen, setIsFormOpen] = useState(false);
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
 
   const [newPost, setNewPost] = useState({
     id: Posts.length + 1,
@@ -40,6 +40,7 @@ function Share({ searchTerm }) {
         like: 0,
         comment: 0,
       });
+      setImageUrl("");
       setIsFormOpen(false);
     }
   };
@@ -50,48 +51,22 @@ function Share({ searchTerm }) {
       [name]: value,
     }));
   };
+  const loadNewImage = () => {
+    const newImageUrl =
+      "https://pivkomarket.kz/wp-content/uploads/2020/06/akcija-31-picca.jpg";
+    setImageUrl(newImageUrl);
+    setNewPost((prevPost) => ({
+      ...prevPost,
+      photo: newImageUrl,
+    }));
+  };
 
-//   const handleContentButtonClick = (content) => {
-//     setNewPost((prevPost) => ({
-//       ...prevPost,
-//       desc: `${prevPost.desc} ${content}`,
-//     }));
-//   };
-    const handleAddPost = () => {
-        if (newPost.desc.trim() !== '') {
-            Posts.push({
-                ...newPost,
-                date: new Date().toLocaleDateString(),
-            });
-            setNewPost({
-                id: Posts.length + 1,
-                userId: 1,
-                header: '',
-                desc: '',
-                photo: '',
-                date: '',
-                like: 0,
-                comment: 0,
-            });
-            setImageUrl('');
-            setIsFormOpen(false);
-        }
-    };
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewPost((prevPost) => ({
-            ...prevPost,
-            [name]: value,
-        }));
-    };
-    const loadNewImage = () => {
-        const newImageUrl = 'https://pivkomarket.kz/wp-content/uploads/2020/06/akcija-31-picca.jpg';
-        setImageUrl(newImageUrl);
-        setNewPost((prevPost) => ({
-            ...prevPost,
-            photo: newImageUrl,
-        }));
-    };
+  const handleContentButtonClick = (content) => {
+    setNewPost((prevPost) => ({
+      ...prevPost,
+      desc: `${prevPost.desc} ${content}`,
+    }));
+  };
 
   const handleNewsContainerClick = () => {
     setIsFormOpen(!isFormOpen);
@@ -108,105 +83,51 @@ function Share({ searchTerm }) {
     );
   });
 
-//   return (
-//     <div className="share">
-//       <div className="share-wrapper">
-//         <div className="news-container" onClick={handleNewsContainerClick}>
-//           <div className="news-wrapper">
-//             <div className="news-top">
-//               <h1>HomePage</h1>
-//             </div>
-//             <div className="news-bottom">
-//               <div className="news-options">
-//                 <button
-//                   className="news-button"
-//                   onClick={() => handleContentButtonClick("📷")}
-//                 >
-//                   <PermMedia className="news-icon" />
-//                 </button>
-//                 <button
-//                   className="news-button"
-//                   onClick={() => handleContentButtonClick("Label")}
-//                 >
-//                   <Label className="news-icon" />
-//                 </button>
-//                 <button
-//                   className="news-button"
-//                   onClick={() => handleContentButtonClick("📍")}
-//                 >
-//                   <Room className="news-icon" />
-//                 </button>
-//                 <button
-//                   className="news-button"
-//                   onClick={() => handleContentButtonClick("😊")}
-//                 >
-//                   <EmojiEmotions className="news-icon" />
-//                 </button>
-//               </div>
-//               <button className="news-button">
-//                 <Telegram />
-//               </button>
-//               {isFormOpen && (
-//                 <div>
-//                   <button className="news-button-save" onClick={handleAddPost}>
-//                     Save
-//                   </button>
-    return(
-        <div className="share">
-            <div className="share-wrapper">
-                <div className="news-container" >
-                    <div className="news-wrapper">
-                        <div className="news-top">
-                            <h1>HomePage</h1>
-                        </div>
-                        <div className="news-bottom">
-                            <div className="news-options">
-                                <button className="news-button" onClick={loadNewImage}>
-                                    <PermMedia className="news-icon"/>
-                                </button>
-                                <button className="news-button" onClick={() => handleContentButtonClick('Label')}>
-                                    <Label className="news-icon"/>
-                                </button>
-                                <button className="news-button" onClick={() => handleContentButtonClick('📍')}>
-                                    <Room className="news-icon"/>
-                                </button>
-                                <button className="news-button" onClick={() => handleContentButtonClick('😊')}>
-                                    <EmojiEmotions className="news-icon"/>
-                                </button>
-                            </div>
-                            <button className="news-button"><Telegram/></button>
-                            {isFormOpen && (
-                            <div>
-                                <button className="news-button-save" onClick={handleAddPost}>
-                                    Save
-                                </button>
-                            </div>
-                                )}
-                        </div>
-                    </div>
-                    <div className="post" onClick={handleNewsContainerClick}>
-                        <div className="post-form">
-                            <input className="post-header"
-                                   type="text"
-                                   placeholder="Post Header"
-                                   name="header"
-                                   value={newPost.header}
-                                   onChange={handleInputChange}
-                            />
-                            <input className="post-desc"
-                                      placeholder="Post Description"
-                                      name="desc"
-                                      value={newPost.desc}
-                                      onChange={handleInputChange}
-                            />
-                            {imageUrl && <img src={imageUrl} alt="Dynamic Image" />}
-                        </div>
-                    </div>
+  return (
+    <div className="share">
+      <div className="share-wrapper">
+        <div className="news-container">
+          <div className="news-wrapper">
+            <div className="news-top">
+              <h1>HomePage</h1>
+            </div>
+            <div className="news-bottom">
+              <div className="news-options">
+                <button className="news-button" onClick={loadNewImage}>
+                  <PermMedia className="news-icon" />
+                </button>
+                <button
+                  className="news-button"
+                  onClick={() => handleContentButtonClick("Label")}
+                >
+                  <Label className="news-icon" />
+                </button>
+                <button
+                  className="news-button"
+                  onClick={() => handleContentButtonClick("📍")}
+                >
+                  <Room className="news-icon" />
+                </button>
+                <button
+                  className="news-button"
+                  onClick={() => handleContentButtonClick("😊")}
+                >
+                  <EmojiEmotions className="news-icon" />
+                </button>
+              </div>
+              <button className="news-button">
+                <Telegram />
+              </button>
+              {isFormOpen && (
+                <div>
+                  <button className="news-button-save" onClick={handleAddPost}>
+                    Save
+                  </button>
                 </div>
               )}
             </div>
           </div>
-          {isFormOpen && (
+          <div className="post" onClick={handleNewsContainerClick}>
             <div className="post-form">
               <input
                 className="post-header"
@@ -216,15 +137,16 @@ function Share({ searchTerm }) {
                 value={newPost.header}
                 onChange={handleInputChange}
               />
-              <textarea
+              <input
                 className="post-desc"
                 placeholder="Post Description"
                 name="desc"
                 value={newPost.desc}
                 onChange={handleInputChange}
               />
+              {imageUrl && <img src={imageUrl} alt="Dynamic Image" />}
             </div>
-          )}
+          </div>
         </div>
         {filteredPosts.map((p) => (
           <Slider key={p.id} post={p} />
